e0d26bb6e1e0af40be3d357bc1ea9dc3b83c02ca
Ilkin Abdullayev <ilkin.abd@inbox.ru>
2026-08-13 16:26:05 +0400

    feat: parameterize database password in docker-compose template via environment variables

--- production/generate_envs.py
+++ production/generate_envs.py
@@ -150,6 +150,7 @@ def main():
         template = template.replace('__REPORT_DOMAIN__', services['REPORT']['domain'])
         template = template.replace('__SOCKET_DOMAIN__', services['SOCKET']['domain'])
         template = template.replace('__API_JWT_SECRET__', env.get('API_JWT_SECRET', ''))
+        template = template.replace('__API_DB_PASSWORD__', env.get('API_DB_PASSWORD', 'postgres'))
         
         with open(compose_output, 'w') as f:
             f.write(template)
